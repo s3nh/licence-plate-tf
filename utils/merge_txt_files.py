12f4f8ad8521616bf6e9a681104028b660189454
s3nh <steam.panek@gmail.com>
2019-09-29 15:38:59 -0400

    Create labels.csv

--- utils/merge_txt_files.py
+++ utils/merge_txt_files.py
@@ -20,14 +20,23 @@ def get_files(_path, extension, outfile):
     out_list = []
     os.chdir(_path)
     _files = glob.glob(extension)
-    for _file in _files:
-        with open(_file, 'r') as f:
-            _tmp = [line.split('\t') for line in f]
-            _tmp.append(out_list)
-    return out_list
+    with open(outfile, 'w') as o:
+        for _file in _files:
+            with open(_file, 'r') as f:
+                _tmp = [line.split('\t') for line in f]
+                _tmp = [el for sublist in _tmp for el in sublist]
+                o.write('{}\n'.format(','.join([el for el in _tmp] )))
+    o.close()
 
 
-
-
+def main():
+    PATH = 'dataset/benchmarks/endtoend/eu'
+    get_files(PATH, '*.txt', outfile='outpath.csv')
+    _file = pd.read_csv('outpath.csv')
+   
+    _file.columns = ['id', 'height', 'width', 'th', 'tw', 'licence' ]
+    _file.to_csv('labels.csv')  
+if __name__ == "__main__":
+    main()    
     
     
