1b23742e91688f20c2b5f4eaf9c3979f216f9754
s3nh <steam.panek@gmail.com>
2019-10-28 16:29:52 -0400

    Proper inference

--- src/inference_IE.py
+++ src/inference_IE.py
@@ -1,6 +1,7 @@
+import time
 import cv2 
 import logging as log 
-from time import time 
+import time
 from openvino.inference_engine import IENetwork, IEPlugin
 
 model_xml = 'tf-mobilenet/training/frozen_inference_graph.xml'
@@ -10,20 +11,37 @@ image = cv2.imread(image_path)
 image = cv2.resize(image, (600, 600))
 print(image.size)
 
+print("Start infer loading time {}".format(time.time()))
+now = time.time()
 blob = cv2.dnn.blobFromImage(image)
 plugin = IEPlugin(device="MYRIAD", plugin_dirs=None)
 net = IENetwork(model = model_xml, weights = model_bin)
 n, c, h, w = net.inputs['image_tensor'].shape
 out_blob = net.outputs
 print(out_blob)
+print("TIme passed {}".format(time.time() - now))
 # Loading model to the plugin 
 
 
+
+print("Start infer loading time {}".format(time.time()))
+infer_time = time.time()
 exec_net = plugin.load(network = net) 
+print("inference time  passed {}".format(time.time() - infer_time))
+
+pred_time = time.time()
 res = exec_net.infer(inputs = {'image_info' : [h, w, 1], 
                                'image_tensor' : blob 
                                })
-
-
-res = res['detection_output']
+print("Prediction time passed {}".format(time.time() - pred_time))
+res = res['detection_output'][0][0][0]
 print(res)
+xmin = int(res[3] * 600)
+xmax = int(res[5] * 600)
+ymin = int(res[4] * 600)
+ymax = int(res[6] * 600)
+
+print("xmin {} xmax {} ymin {} ymax {}".format(xmin, xmax, ymin, ymax))
+
+licence = image[ymin:ymax, xmin:xmax]
+cv2.imwrite('licence.jpg', licence )
